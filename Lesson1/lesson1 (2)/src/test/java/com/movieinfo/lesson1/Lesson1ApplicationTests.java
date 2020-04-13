b3ea59aab3aa16aa9ccc55c3a5204e9e2c8728c0
abrahamgolla17 <abrahamgolla17@gmail.com>
2020-04-13 22:43:04 +0530

    first commit

---
+++ Lesson1/lesson1 (2)/src/test/java/com/movieinfo/lesson1/Lesson1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movieinfo.lesson1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lesson1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
